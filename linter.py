5d2cf873077afad5748ecd61903469c0d7230fbb
Dave O'Brien <dave.2pi@gmail.com>
2017-12-21 11:35:51 +0000

    Update regex

--- linter.py
+++ linter.py
@@ -8,7 +8,7 @@
 # License: MIT
 #
 
-"""This module exports the vlog plugin class."""
+"""Exports the vlog plugin class."""
 
 from SublimeLinter.lint import Linter
 
@@ -23,16 +23,20 @@ class Vlog(Linter):
     # SAMPLE ERRORS
     # ** Error: (vcom-13069) .\fname.v(9): near "reg": syntax error, unexpected reg, expecting ';' or ','.
     # ** Error: (vcom-13069) .\fname.v(9): Unknown identifier "var": syntax error, unexpected reg, expecting ';' or ','.
+    # ** Error (suppressible): .\fname.sv(46): (vlog-2388) 'var' already declared in this scope (mname).
+    # ** Error: (vlog-13069) .\fname.sv(45): near "==": syntax error, unexpected ==, expecting ';' or ','.
 
     regex = (
-        r'^\*\* ((?P<error>Error: )|(?P<warning>Warning: ))'   # Error
+        r'^\*\* (((?P<error>Error)|(?P<warning>Warning))'      # Error
+        r'( \(suppressible\))?: )'                             # Maybe suppressible
         r'(\([a-z]+-[0-9]+\) )?'                               # Error code - sometimes before
         r'([^\(]*\((?P<line>[0-9]+)\): )'                      # File and line
         r'(\([a-z]+-[0-9]+\) )?'                               # Error code - sometimes after
-        r'((near|Unknown identifier|Undefined variable):? '    # Near/Unidentified
-        r'["\'](?P<near>[\w:;\.]+)["\']'                       # Identifier
+        r'(?P<message>'                                        # Start of message
+        r'(((near|Unknown identifier|Undefined variable):? )?' # Near/Unknown/Unidentified
+        r'["\'](?P<near>[\w=:;\.]+)["\']'                      # Identifier
         r'[ :.]*)?'                                            # Near terminator
-        r'(?P<message>.*)'                                     # Remaining message
+        r'.*)'                                                 # End of message
     )
 
     def split_match(self, match):
@@ -41,3 +45,7 @@ class Vlog(Linter):
         if match:
             message = '[vlog] ' + message
         return match, line, col, error, warning, message, near
+
+"""
+^\*\* (((Error)|(Warning))( \(suppressible\))?: )(\([a-z]+-[0-9]+\) )?([^\(]*\(([0-9]+)\): )(\([a-z]+-[0-9]+\) )?((((near|Unknown identifier|Undefined variable):? )?["']([\w:;\.]+)["'][ :.]*)?.*)
+"""
